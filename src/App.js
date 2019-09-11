31d41782f55364dc14afb0483867061fbffd2186
huahua0406 <623072902@qq.com>
2019-09-11 22:37:24 +0800

    update

--- src/App.js
+++ src/App.js
@@ -8,12 +8,18 @@ import enUS from 'antd/es/locale/en_US';
 import zhCN from 'antd/es/locale/zh_CN';
 import moment from 'moment';  // antd 框架中内置了 moment 库
 import 'moment/locale/zh-cn';
-moment.locale('zh-cn');
 // 全局对象
 import config from './config/config';
 import api from './api';
 // App css
-import './App.css'; 
+import './App.css';
+
+// 路由
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import BasicLayout from '@/layouts/BasicLayout/BasicLayout';
+import Login from '@/pages/Login/Login';
+import NotFound from '@/pages/Error/NotFound';
+moment.locale('zh-cn'); 
 /**
  *  Api 全局变量
  *  App.js -> window.Api = api
@@ -21,18 +27,12 @@ import './App.css';
 window.Api = api
 window.Config = config
 
-// 路由
-import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import BasicLayout from '@/layouts/BasicLayout/BasicLayout';
-import Login from '@/pages/Login/Login';
-import NotFound from '@/pages/Error/NotFound';
-
 
 function App() {
     return (
-        <ConfigProvider locale={zhCN}>
-            <Provider store={store}>
-                    <div className="App">
+        <Provider store={store}>
+            <ConfigProvider locale={zhCN}>
+                    <main className="App">
                         <Router>
                             <Switch>
                                 <Route exact path='/' render={() => <Redirect to={`/home`} push/>}/>
@@ -41,9 +41,9 @@ function App() {
                                 <Route component={NotFound}/>
                             </Switch>
                         </Router>
-                    </div>
-            </Provider>
-        </ConfigProvider>
+                    </main>
+                </ConfigProvider>
+        </Provider>
     );
 }
 
